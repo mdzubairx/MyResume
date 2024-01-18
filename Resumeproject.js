0536b171b6716420c13857b402db1cbdfe9ed3ab
zubair01-coder <110790235+zubair01-coder@users.noreply.github.com>
2024-01-19 01:23:06 +0530

    Add files via upload

---
+++ Resumeproject.js
@@ -0,0 +1,18 @@
+var navmenutags = document.querySelectorAll('.nav-menu a');
+for (i=0; i<navmenutags.length; i++){
+    navmenutags[i].addEventListener('click',function(event){
+        event.preventDefault();
+        var targetsectionID = this.textContent.trim().toLowercase();
+        var targetSection = document.getElementById(targetsectionID);
+        console.log(targetSection);
+      
+      var interval = setInterval(function(){
+        var targetSectionCoordinates = targetSection.getBoundingClientRect();
+        if(targetSectionCoordinates.top <= 0){
+        clearInterval(interval);
+        return;
+      }
+      window.scrollBy(0, 50);
+        },50 );  
+    });
+}
